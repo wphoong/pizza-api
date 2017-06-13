07ec50e9ce0b99e3faa9bd95a8c7959ab4e24e52
Wilson <wphoong@gmail.com>
2017-06-13 18:31:14 +0000

    configured rspec for all route

--- spec/controllers/all_controller_spec.rb
+++ spec/controllers/all_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe AllController, type: :controller do
-
+  context '/all' do
+    it 'should list all ingredients in database' do
+      crust = FactoryGirl.create(:crust)
+      sauce = FactoryGirl.create(:sauce)
+      get :index
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value.count).to eq 2
+    end
+  end
 end
